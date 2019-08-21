20d3a379dd4682f4c9f57f239dbf764a525bf02a
Florian Blasius <florian.blasius@ergosign.de>
2019-08-21 21:37:16 +0200

    Implement cargo web installation.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::process::{Command, Stdio};
+
 fn main() {
-    println!("Hello, cargo-orbtk!");
+    if let Ok(output) = Command::new("cargo").arg("web").output() {
+        if !output.status.success() {
+            Command::new("cargo")
+                .arg("install")
+                 .arg("--color")
+                 .arg("always")
+                .arg("cargo-web")
+                .stderr(Stdio::inherit())
+                .stdin(Stdio::null())
+                .stdout(Stdio::piped())
+                .output()
+                .expect("Could not install cargo-web");
+
+            let output = String::from_utf8_lossy(&output.stdout).into_owned();
+
+            println!("{}", output);
+        }
+    } else {
+        Command::new("cargo")
+            .arg("install")
+            .arg("cargo-web")
+            .output()
+            .expect("Could not install cargo-web");
+    }
 }
